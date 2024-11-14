488f360407316469f63e74e0f10f7bc37482a054
Glitch (glitch-blank-website) <none>
2024-11-14 01:33:28 +0000

    🙎🍧 Checkpoint ./script.js:70518738/1034

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// function for our list view
+async function fetchProducts() {
+  let getResultElement = document.getElementById("brews");
 
-console.log("Hello, world!");
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `patalfq5yroYtu7wV.77b794fc190f162598e9f93cc9207da0fdcd337901d7694394a4a1085cc0524c`,
+    },
+  };
+
+  await fetch(
+    `https://api.airtable.com/v0/appVWWY9vo2FICye0/Furnitures \?&view=Stars`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data); // response is an object w/ .records array
