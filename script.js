fed6765e5f4f36235573bb2e68e6f6674c85f933
Glitch (glitch-blank-website) <none>
2024-11-14 01:44:30 +0000

    🏚👨‍👨‍👦 Checkpoint ./product.html:70518738/138 ./script.js:70518738/2992

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+"use strict";
+
 // function for our list view
 async function fetchProducts() {
-  let getResultElement = document.getElementById("brews");
+  let getResultElement = document.getElementById("product-list");
 
   const options = {
     method: "GET",
     headers: {
-      Authorization: `patalfq5yroYtu7wV.77b794fc190f162598e9f93cc9207da0fdcd337901d7694394a4a1085cc0524c`,
+      Authorization: `Bearer patalfq5yroYtu7wV.77b794fc190f162598e9f93cc9207da0fdcd337901d7694394a4a1085cc0524c`,
     },
   };
 
@@ -16,3 +18,37 @@ async function fetchProducts() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data); // response is an object w/ .records array
+
+      getResultElement.innerHTML = ""; // clear brews
+
+      let newHtml = "";
+
+      for (let i = 0; i < data.records.length; i++) {
+        let logo = data.records[i].fields["name"]; // here we are getting column values
+        let name = data.records[i].fields["images"];
+        let neighborhood = data.records[i].fields["Neighborhood"];
+
+        newHtml += `
+        
+         <div class="col-xl-4 cardImageText">
+          <div class="card list move border-dark mb-5" style="width: 20rem;">
+          <a href="breweries.html?id=${
+            data.records[i].id
+          }">${
+          logo
+            ? `<img class="card-img-top rounded" alt="${name}" src="${logo[0].url}">`
+            : ``
+        }
+          </a>
+          <p hidden class="card-key">${neighborhood}</p>
+          </div>
+          </div>
+        </div>
+        
+        
+        `;
+      }
+
+      getResultElement.innerHTML = newHtml;
+    });
+}
